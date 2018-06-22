8977bcdff1336bc21d5a06d272d2ddba63059818
Allan Burns <allanlburns@yahoo.com>
2018-06-21 20:53:50 -0400

    Create bullet.py and write behavior for bullets. Add press 'q' to quit function.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -13,14 +14,17 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
     
     # Set the background color.
     bg_color = (230, 230, 230)
     
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
-        ship.update() 
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
 run_game()
