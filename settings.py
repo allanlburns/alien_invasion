8b6c90f5cf12db803899f219718917b2aad7ea7d
Allan Burns <allanlburns@yahoo.com>
2018-06-23 18:24:43 -0400

    Repopulate fleet when all aliens destroyed and speed up bullets.

--- settings.py
+++ settings.py
@@ -12,11 +12,11 @@ class Settings():
 		self.ship_speed_factor = 1.5
 
 		# Bullet settings
-		self.bullet_speed_factor = 1
+		self.bullet_speed_factor = 3
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
-		self.bullets_allowed = 3
+		self.bullets_allowed = 5
 
 		# Alien settings
 		self.alien_speed_factor = 1
