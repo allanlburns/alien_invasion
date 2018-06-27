3978e3a5728087163b460814c8adb5e60d9fa45c
Allan Burns <allanlburns@yahoo.com>
2018-06-26 20:52:57 -0400

    Add high score, level, remaining ships.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings():
 		self.ship_limit = 3
 
 		# Bullet settings
-		self.bullet_width = 3
+		self.bullet_width = 500
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
 		self.bullets_allowed = 5
@@ -21,7 +21,9 @@ class Settings():
 		self.fleet_drop_speed = 10
 
 		# How quickly the game speeds up
-		self.speedup_scale = 1.1
+		self.speedup_scale = 1.2
+		# How quickly the alien point values increase
+		self.score_scale = 1.5
 
 		self.initialize_dynamic_settings()
 
@@ -38,7 +40,10 @@ class Settings():
 		self.alien_points = 50
 
 	def increase_speed(self):
-		"""Increase speed settings."""
+		"""Increase speed settings and alien point values."""
 		self.ship_speed_factor *= self.speedup_scale
 		self.bullet_speed_factor *= self.speedup_scale
 		self.alien_speed_factor *= self.speedup_scale
+
+		self.alien_points = int(self.alien_points * self.score_scale)
+		#print(self.alien_points)
